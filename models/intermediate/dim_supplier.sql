67d8a4fe581567811fd800a4d6ca239759c647bb
phucnguyen20110276 <112325531+phucnguyen20110276@users.noreply.github.com>
2025-10-13 23:54:24 +0700

    Create dim_supplier.sql

---
+++ models/intermediate/dim_supplier.sql
@@ -0,0 +1,24 @@
+-- depends_on: {{ ref('stg_supplier') }}
+-- depends_on: {{ ref('stg_nation') }}
+-- depends_on: {{ ref('stg_region') }}
+
+with supplier as (
+  select * 
+  from {{ ref('stg_supplier') }}
+),
+nation as (
+  select *
+  from {{ ref('stg_nation') }}
+),
+region as (
+  select *
+  from {{ ref('stg_region') }}
+)
+
+select 
+  s.supplier_key,
+  n.nation_name,
+  r.region_name
+from supplier as s
+join nation as n on n.nation_key = s.nation_key
+join region as r on r.region_key = n.region_key
