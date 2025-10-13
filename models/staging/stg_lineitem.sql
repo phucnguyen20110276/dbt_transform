675bb809938e7f6b1a23453f240d85771643ddb1
phucnguyen20110276 <112325531+phucnguyen20110276@users.noreply.github.com>
2025-10-13 18:39:30 +0700

    Update stg_lineitem.sql

--- models/staging/stg_lineitem.sql
+++ models/staging/stg_lineitem.sql
@@ -7,7 +7,7 @@ select
   l_orderkey as order_key,
   l_partkey as part_key,
   l_suppkey as supplier_key,
-  l_lineitem as lineitem,
+  l_linenumber as line_number,
   l_quantity as quantity,
   l_extendedprice as extended_price,
   l_discount as discount,
