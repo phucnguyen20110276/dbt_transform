df9207b1a551fd29da72bd8ce730ec2ccb546eac
phucnguyen20110276 <112325531+phucnguyen20110276@users.noreply.github.com>
2025-10-13 20:50:03 +0700

    Update stg_part.sql

--- models/staging/stg_part.sql
+++ models/staging/stg_part.sql
@@ -5,6 +5,8 @@ with part as (
 
 select
   p_partkey as part_key,
-  p_suppkey as supplier_key,
-  p_supplycost as supply_cost
+  p_mfgr as manufacturer,
+  p_brand as brand,
+  p_size as size,
+  p_retailprice as retail_price
 from part
