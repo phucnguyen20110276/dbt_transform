283ec6ad2e54f29ae962f13a804ff6e5436b7f2a
phucnguyen20110276 <112325531+phucnguyen20110276@users.noreply.github.com>
2025-10-14 22:12:16 +0700

    Update dim_customer.sql

--- models/intermediate/dim_customer.sql
+++ models/intermediate/dim_customer.sql
@@ -18,8 +18,8 @@ region as (
 select
   c.customer_key,
   c.market_segmentation,
-  n.nation_name,
-  r.region_name
+  n.nation_name as customer_nation,
+  r.region_name as customer_region
 from customer as c
 join nation as n on n.nation_key = c.nation_key
 join region as r on r.region_key = n.region_key
