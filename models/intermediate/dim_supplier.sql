6929e7d9f9f4f1bf9bc2e6b730d2b724c554fcf3
phucnguyen20110276 <112325531+phucnguyen20110276@users.noreply.github.com>
2025-10-14 22:03:54 +0700

    Update dim_supplier.sql

--- models/intermediate/dim_supplier.sql
+++ models/intermediate/dim_supplier.sql
@@ -17,8 +17,8 @@ region as (
 
 select 
   s.supplier_key,
-  n.nation_name,
-  r.region_name
+  n.supplier_nation_name,
+  r.supplier_region_name
 from supplier as s
 join nation as n on n.nation_key = s.nation_key
 join region as r on r.region_key = n.region_key
